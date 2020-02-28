537065e5a1a7148e2d1c9e121ca009250de69998
rf-spuds <rfspuds@gmail.com>
2020-02-27 20:09:09 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+Outline / Pseudo code
+for HW - week 4
+    // Create Q&A Object.  Maybe an array? 
+    // ex. Q&A = [
+    //   { q: " what is this?" 
+    //      1: " "
+    //      2: " "
+    //      3: " "
+    //      4: " "
+    //      a: 2 }
+    //       ]
+    // Create Start Button (HTML)
+    // Attach a click event 
+    // Start timer countdown (recommend using the setInterval method) 
+    // Generate first question + answers + data attributes 
+    // add "click" event for answer buttons 
+    // increment a counter
+    // Generate question 
+    // if incorrect 
+    //deduct time counter
