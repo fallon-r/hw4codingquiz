1f5fc4f7bacabeaea61f5006d22c1d132ce189ff
rf-spuds <rfspuds@gmail.com>
2020-03-01 17:15:32 -0500

    added question incrementer base

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@
 $("#startBtn").click(function(event) {
     // remove the start button
     $("#startBtn").remove();
+    // timer creation
     var time_in_minutes = 2;
     var current_time = Date.parse(new Date());
     var deadline = new Date(current_time + time_in_minutes * 60 * 1000);
@@ -13,7 +14,9 @@ $("#startBtn").click(function(event) {
         var minutes = Math.floor((t / 1000 / 60) % 60);
         var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
         var days = Math.floor(t / (1000 * 60 * 60 * 24));
-        return { 'total': t, 'days': days, 'hours': hours, 'minutes': minutes, 'seconds': seconds };
+
+        // adding slice to seconds to make sure there's a 0 when the numbers's <10
+        return { 'total': t, 'days': days, 'hours': hours, 'minutes': minutes, 'seconds': ('0' + seconds).slice(-2) };
     }
 
     function run_clock(id, endtime) {
@@ -22,15 +25,26 @@ $("#startBtn").click(function(event) {
         function update_clock() {
             var t = time_remaining(endtime);
             clock.innerHTML = t.minutes + ':' + t.seconds;
+
             if (t.total <= 0) { clearInterval(timeinterval); }
         }
-        update_clock(); // run function once at first to avoid delay
+        update_clock();
         var timeinterval = setInterval(update_clock, 1000);
     }
     run_clock('#timerDisplay', deadline);
 
+    $("#questionNumber").append(1);
 
 });
+
+// submit button 
+$("#submit").click(function(event) {
+    for (var i = 0; i > 11; i++) {
+        i = i++;
+        $("#questionNumber").append(i);
+
+    }
+})
 
 
 
@@ -45,6 +59,8 @@ $("#startBtn").click(function(event) {
 //      4: " "
 //      a: 2 }
 //       ]
+
+var qArray = ["What color is the sky?"]
 
 
 // Generate first question + answers + data attributes 
