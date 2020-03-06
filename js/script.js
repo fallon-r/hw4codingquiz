f6cf1b57371a11fdb233896b9c0d4472b0d0b555
rf-spuds <rfspuds@gmail.com>
2020-03-05 22:52:12 -0500

    cannot get timer to go down ten seconds on click.

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ var qArray = [{
         a: "Brendan Eich",
         b: "Bill Gates",
         c: "Steve Jobs",
-        d: "Richard Stallman"
+        d: "Richard Stallman",
     },
     {
         question: "What is the command to clear a console?",
@@ -49,17 +49,17 @@ var qArray = [{
         c: "array.split()",
         d: "array.spin()"
     }, {
-        question: "CSS stands for:",
-        a: "Cascading Style Sandwich",
-        b: "Cataclysmic Style Synthesizer",
-        c: "Coronal Stylizing System",
-        d: "Cascading Style Sheet"
-    }, {
         question: "If (i != 0) runs when",
         a: "i is NOT equal to 0",
         b: "i is equal to 0",
         c: "i = zero",
         d: "i is undefined"
+    }, {
+        question: "CSS stands for:",
+        a: "Cascading Style Sandwich",
+        b: "Cataclysmic Style Synthesizer",
+        c: "Coronal Stylizing System",
+        d: "Cascading Style Sheet"
     }, {
         question: "if (i > 4 && n < 7) will run when",
         a: "i is 5",
@@ -69,7 +69,16 @@ var qArray = [{
     }
 
 ];
+
+var t;
+var seconds;
+var deadline;
+var time_in_seconds;
+
+// Makes iterating through the clock easier 
 var countForI = 0;
+
+// global variables to manipulate timer on multiple click events
 
 // game dead till iniitialized
 $("#submit").prop('disabled', true);
@@ -88,20 +97,20 @@ $("#startBtn").click(function(event) {
     // remove the start button
     $("#startBtn").remove();
     // timer creation
-    var time_in_minutes = 2;
+    time_in_seconds = 120;
     var current_time = Date.parse(new Date());
-    var deadline = new Date(current_time + time_in_minutes * 60 * 1000);
+
 
 
     function time_remaining(endtime) {
-        var t = Date.parse(endtime) - Date.parse(new Date());
-        var seconds = Math.floor((t / 1000) % 60);
-        var minutes = Math.floor((t / 1000 / 60) % 60);
-        var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
-        var days = Math.floor(t / (1000 * 60 * 60 * 24));
+        t = time_in_seconds--;
+        deadline = 120 - t;
+        // seconds = Math.floor((t / 1000));
+
+
 
         // adding slice to seconds to make sure there's a 0 when the numbers's <10
-        return { 'total': t, 'days': days, 'hours': hours, 'minutes': minutes, 'seconds': ('0' + seconds).slice(-2) };
+        return t;
     }
 
     function run_clock(id, endtime) {
@@ -109,12 +118,13 @@ $("#startBtn").click(function(event) {
 
         function update_clock() {
             var t = time_remaining(endtime);
-            clock.innerHTML = t.minutes + ':' + t.seconds;
+            clock.innerHTML = t;
 
-            if (t.total <= 0) { clearInterval(timeinterval); }
+            if (t <= 0) { clearInterval(timeinterval); }
         }
         update_clock();
         var timeinterval = setInterval(update_clock, 1000);
+
     }
     run_clock('#timerDisplay', deadline);
 
@@ -168,6 +178,9 @@ $("#startBtn").click(function(event) {
         console.log("you clicked D")
     });
 
+    // if () {
+    //     alert("You're done bro")
+    // }
 
 
 });
@@ -194,11 +207,12 @@ $("#submit").click(function(event) {
         $("#A, #B, #C, #D").addClass("btn-dark");
     } else {
         alert("You're done, son")
+        stopTimer();
+
     }
 
     // checks if question number is <10
-
-
+    $("#timerDisplay").text(t - 10);
 
 });
 
